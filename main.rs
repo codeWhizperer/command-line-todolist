957e8222b7b28c6d061e63618d4abc8dd1767976
Adegbite Ademola Kelvin <Adegbiteademola1999@gmail.com>
2023-04-05 21:57:27 +0100

    rust terminal todo-list

---
+++ main.rs
@@ -0,0 +1,26 @@
+use structopt::StructOpt;
+mod cli;
+mod tasks;
+
+use cli::{Action::*, CommandLineArgs};
+use tasks::Task;
+
+fn main() {
+    // Get the command-line arguments.
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+
+    // Unpack the journal file.
+    let journal_file = journal_file.expect("Failed to find journal file");
+
+    // Perform the action.
+    match action {
+        Add { text } => tasks::add_task(journal_file, Task::new(text)),
+        List => tasks::list_tasks(journal_file),
+        ListTask{position} => tasks::list_a_task(journal_file,position),
+        Done { position } => tasks::complete_task(journal_file, position),
+    }
+    .expect("Failed to perform action")
+}
